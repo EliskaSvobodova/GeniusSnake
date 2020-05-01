20450cc697a2d339fe20f5d6ee07e09229d8a7ae
svoboel5 <svoboel5@fit.cvut.cz>
2020-05-01 19:50:48 +0200

    Add Controller module basics
    - abstract class + 2 derived classes
    - one for human player, one for genetic programming

---
+++ Controller.py
@@ -0,0 +1,13 @@
+from abc import ABCMeta, abstractmethod
+
+
+class AbstractController(metaclass=ABCMeta):
+    pass
+
+
+class PlayerController(AbstractController):
+    pass
+
+
+class GeneticController(AbstractController):
+    pass
