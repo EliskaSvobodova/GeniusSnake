c055e1b9f3aaadb3af423570ce5f64571408822d
svoboel5 <svoboel5@fit.cvut.cz>
2020-05-01 19:49:39 +0200

    Add action to be done after clicking play button

--- Menu.py
+++ Menu.py
@@ -1,39 +1,37 @@
 import pyglet
 import resources
-
-
-def configure_resources():
-    pyglet.resource.path = ['resources']
-    pyglet.resource.reindex()
-
-
-def center_image(image):
-    image.anchor_x = image.width // 2
-    image.anchor_y = image.height // 2
+import Game
+import Controller
+import UI
+import CommonHelpers
 
 
 class Menu:
     def __init__(self):
-        self.set_menu_window()
-        configure_resources()
+        self.set_window()
+        CommonHelpers.configure_resources()
         self.load_labels()
         self.load_images()
 
-    def set_menu_window(self):
+    @classmethod
+    def run_application(cls):
+        pyglet.app.run()
+
+    def set_window(self):
         display = pyglet.canvas.Display()
         screen = display.get_default_screen()
         self.screen_width = screen.width
         self.screen_height = screen.height
-        self.menu_window = pyglet.window.Window(self.screen_width, self.screen_height)
-        self.menu_window.on_draw = self.on_menu_window_draw
-        self.menu_window.on_mouse_press = self.on_mouse_press
-        self.menu_window.on_mouse_motion = self.on_mouse_motion
+        self.window = pyglet.window.Window(self.screen_width, self.screen_height)
+        self.window.push_handlers(on_draw=self.on_menu_draw,
+                                  on_mouse_press=self.on_menu_mouse_press,
+                                  on_mouse_motion=self.on_menu_mouse_motion)
 
     def load_images(self):
         snake_image = pyglet.resource.image("snake_background.jpg")
         button_background_image = pyglet.resource.image("button_background.png")
-        center_image(snake_image)
-        center_image(button_background_image)
+        CommonHelpers.center_image(snake_image)
+        CommonHelpers.center_image(button_background_image)
         self.snake = pyglet.sprite.Sprite(img=snake_image, x=(self.screen_width // 2), y=(self.screen_height // 2))
         self.button_background = pyglet.sprite.Sprite(img=button_background_image,
                                                       x=self.play_label.x, y=self.play_label.y)
@@ -57,31 +55,31 @@ class Menu:
                                             anchor_x="center", anchor_y="center",
                                             font_name="Bangers", font_size=50)
 
-    @classmethod
-    def run_application(cls):
-        pyglet.app.run()
+    def mouse_on_play_button(self, x, y):
+        if ((self.button_background.x - (self.button_background.width // 2) < x)
+                & (x < self.button_background.x + (self.button_background.width // 2))
+                & (self.button_background.y - (self.button_background.height // 2) < y)
+                & (y < self.button_background.y + (self.button_background.height // 2))):
+            return True
+        return False
 
-    def on_menu_window_draw(self):
+    def on_menu_draw(self):
         self.snake.draw()
         self.game_name_shadow_label.draw()
         self.game_name_label.draw()
         self.button_background.draw()
         self.play_label.draw()
 
-    def mouse_on_play_button(self, x, y):
-        if ((self.button_background.x - (self.button_background.width // 2) < x)
-                & (x < self.button_background.x + self.button_background.width)
-                & (self.button_background.y - (self.button_background.height // 2) < y)
-                & (y < self.button_background.y + self.button_background.height)):
-            return True
-        return False
-
-    def on_mouse_press(self, x, y, button, modifiers):
+    def on_menu_mouse_press(self, x, y, button, modifiers):
         # play clicked
         if self.mouse_on_play_button(x, y):
-            print("PLAY")  # TODO: play button clicked
+            self.window.pop_handlers()
+            self.window.clear()
+            controller = Controller.PlayerController
+            ui = UI.NiceUI(10, 80, self.screen_width - 20, self.screen_height - 90, self.window)
+            game = Game.Game(controller, ui)
 
-    def on_mouse_motion(self, x, y, dx, dy):
+    def on_menu_mouse_motion(self, x, y, dx, dy):
         if self.mouse_on_play_button(x, y):
             self.button_background.opacity = 255
         else:
