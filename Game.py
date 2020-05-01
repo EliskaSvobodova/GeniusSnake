a09dfe6a44b59ac76086a020ea66231b34c4f413
svoboel5 <svoboel5@fit.cvut.cz>
2020-05-01 19:52:37 +0200

    Module for game itelf, connects controllers and ui

---
+++ Game.py
@@ -0,0 +1,20 @@
+import Controller
+import UI
+import Snake
+
+
+class Game:
+    def __init__(self, controller: Controller.AbstractController, ui: UI.AbstractUI):
+        self.controller = controller
+        self.ui = ui
+        self.score = 0
+        self.game_field_height = self.ui.get_num_squares_height()
+        self.game_field_width = self.ui.get_num_squares_width()
+        self.snake = Snake.Snake()
+        self.prepare_game()
+
+    def prepare_game(self):
+        self.ui.draw_boundary()
+        self.ui.draw_background()
+        self.ui.draw_score(self.score)
+        self.ui.draw_snake(self.snake)
