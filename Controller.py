04d44f9ba66e7bd88b82f603f0eb5dc79c5b9ab5
svoboel5 <svoboel5@fit.cvut.cz>
2020-05-04 20:12:10 +0200

    Working: controller(game(ui))

--- Controller.py
+++ Controller.py
@@ -14,40 +14,15 @@ class PlayerController(AbstractController):
         super().__init__()
         self.window = window
         self.game = game
+        self.next_move = Constants.SNAKE_MOVE_FORWARD
+
+        pyglet.clock.schedule_interval(self.control, 1/30)
+
+    def control(self, dt):
+        self.game.play(self.next_move)
 
     def on_key_press(self, symbol, modifiers):
-        if symbol == pyglet.window.key.UP:
-            direction = Snake.heads_direction(self.game.snake.head)
-            if direction is Constants.SNAKE_HEADS_UP:
-                self.game.play(Constants.SNAKE_MOVE_FORWARD)
-            elif direction is Constants.SNAKE_HEADS_LEFT:
-                self.game.play(Constants.SNAKE_MOVE_RIGHT)
-            elif direction is Constants.SNAKE_HEADS_RIGHT:
-                self.game.play(Constants.SNAKE_MOVE_LEFT)
-        if symbol == pyglet.window.key.RIGHT:
-            direction = Snake.heads_direction(self.game.snake.head)
-            if direction is Constants.SNAKE_HEADS_UP:
-                self.game.play(Constants.SNAKE_MOVE_RIGHT)
-            elif direction is Constants.SNAKE_HEADS_DOWN:
-                self.game.play(Constants.SNAKE_MOVE_LEFT)
-            elif direction is Constants.SNAKE_HEADS_RIGHT:
-                self.game.play(Constants.SNAKE_MOVE_LEFT)
-        if symbol == pyglet.window.key.UP:
-            direction = Snake.heads_direction(self.game.snake.head)
-            if direction is Constants.SNAKE_HEADS_UP:
-                self.game.play(Constants.SNAKE_MOVE_FORWARD)
-            elif direction is Constants.SNAKE_HEADS_LEFT:
-                self.game.play(Constants.SNAKE_MOVE_RIGHT)
-            elif direction is Constants.SNAKE_HEADS_RIGHT:
-                self.game.play(Constants.SNAKE_MOVE_LEFT)
-        if symbol == pyglet.window.key.UP:
-            direction = Snake.heads_direction(self.game.snake.head)
-            if direction is Constants.SNAKE_HEADS_UP:
-                self.game.play(Constants.SNAKE_MOVE_FORWARD)
-            elif direction is Constants.SNAKE_HEADS_LEFT:
-                self.game.play(Constants.SNAKE_MOVE_RIGHT)
-            elif direction is Constants.SNAKE_HEADS_RIGHT:
-                self.game.play(Constants.SNAKE_MOVE_LEFT)
+        self.next_move = Constants.SNAKE_MOVE_FORWARD
 
 
 
