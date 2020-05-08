918410776319044ae759d7d3708f576bd205d63f
svoboel5 <svoboel5@fit.cvut.cz>
2020-05-08 15:04:54 +0200

    Basic operators, looping over generations

--- GeneticController.py
+++ GeneticController.py
@@ -1,7 +1,8 @@
 import Game
 import Constants
 import random
-import pyglet
+import copy
+import NoUI
 
 max_depth = 10
 
@@ -20,52 +21,165 @@ class TreeNode:
 
 
 class GeneticController:
+    id = -1
+
+    @classmethod
+    def next_id(cls):
+        cls.id += 1
+        return cls.id
+
     def __init__(self, game: Game.Game, root=None):
         self.game = game
         if root is None:
-            self.root = generate_tree(0, self.game)
+            self.root, self.num_nodes = generate_tree(0, self.game)
         else:
             self.root = root
         self.state = Constants.PLAY
+        self.id = self.next_id()
 
     def make_next_move(self):
         self.game.make_next_move(self.root())
         self.state = self.game.game_state
 
+    def __eq__(self, other):
+        return self.id == other.id
+
+    def crossover(self, other):
+        offs1 = copy.deepcopy(self.root)
+        offs2 = copy.deepcopy(other.root)
+        r1 = random.randint(0, self.num_nodes - 1)
+        r2 = random.randint(0, other.num_nodes - 1)
+        parent_node1, son_type1 = self.find_parent_node(r1, 0, offs1)
+        parent_node2, son_type2 = self.find_parent_node(r2, 0, offs2)
+        # -1:root, 0:left, 1:right
+        if son_type1 == -1:
+            tmp = offs1
+            if son_type2 == -1:
+                pass  # we switch hole offsprings, no effect
+            elif son_type2 == 0:
+                offs1 = parent_node2.left
+                offs2 = tmp
+            elif son_type2 == 1:
+                offs1 = parent_node2.right
+                offs2 = tmp
+        elif son_type1 == 0:
+            tmp = parent_node1.left
+            if son_type2 == -1:
+                parent_node1.left = offs2
+                offs2 = tmp
+            elif son_type2 == 0:
+                parent_node1.left = parent_node2.left
+                parent_node2.left = tmp
+            elif son_type2 == 1:
+                parent_node1.left = parent_node2.right
+                parent_node2.right = tmp
+        elif son_type1 == 1:
+            tmp = parent_node1.right
+            if son_type2 == -1:
+                parent_node1.right = offs2
+                offs2 = tmp
+            elif son_type2 == 0:
+                parent_node1.right = parent_node2.left
+                parent_node2.left = tmp
+            elif son_type2 == 1:
+                parent_node1.right = parent_node2.right
+                parent_node2.right = tmp
+        offspring1 = GeneticController(
+            Game.Game(
+                NoUI.NoUI(0, 0, self.game.ui.width, self.game.ui.height, self.game.ui.square_size)),
+            offs1)
+        offspring1.num_nodes = count_nodes(offspring1.root)
+        offspring2 = GeneticController(
+            Game.Game(
+                NoUI.NoUI(0, 0, self.game.ui.width, self.game.ui.height, self.game.ui.square_size)),
+            offs2)
+        offspring2.num_nodes = count_nodes(offspring2.root)
+        return offspring1, offspring2
+
+    def find_parent_node(self, index, current, tree):
+        if index == current:
+            return tree, -1
+        if tree.left is None:
+            return None, current
+        res, current = self.find_parent_node(index, current + 1, tree.left)
+        if current == -1:
+            return tree, 0
+        if res is not None:
+            return res, current
+        res, current = self.find_parent_node(index, current + 1, tree.right)
+        if current == -1:
+            return tree, 1
+        return res, current
+
+    def find_node(self, index, current, tree):
+        if index == current:
+            return tree, current
+        if tree.left is None:
+            return None, current
+        res, current = self.find_parent_node(index, current + 1, tree.left)
+        if res is not None:
+            return res, current
+        res, current = self.find_parent_node(index, current + 1, tree.right)
+        return res, current
+
+    def mutate(self):
+        r = random.randint(0, self.num_nodes - 1)
+        node, dummy = self.find_node(r, 0, self.root)
+        if node.left is None:
+            node.value = get_random_terminal()
+        else:
+            node.value = get_random_function(self.game)
+
 
 def generate_tree(depth, game):
     if depth == max_depth:
-        return get_random_terminal()
+        return get_random_terminal_tree_node(), 1
     if random.random() < 0.5:
-        fnc = get_random_function(game)
-        fnc.left = generate_tree(depth + 1, game)
-        fnc.right = generate_tree(depth + 1, game)
-        return fnc
+        fnc = get_random_function_tree_node(game)
+        fnc.left, left_nodes = generate_tree(depth + 1, game)
+        fnc.right, right_nodes = generate_tree(depth + 1, game)
+        return fnc, (left_nodes + right_nodes + 1)
     else:
-        return get_random_terminal()
+        return get_random_terminal_tree_node(), 1
 
 
 def generate_test_tree(game):
-    return TreeNode(game.if_obstacle_forward, TreeNode(Constants.SNAKE_MOVE_LEFT), TreeNode(Constants.SNAKE_MOVE_FORWARD))
+    return TreeNode(game.if_obstacle_forward, TreeNode(Constants.SNAKE_MOVE_LEFT),
+                    TreeNode(Constants.SNAKE_MOVE_FORWARD))
 
 
 def get_random_terminal():
-    terminal = random.choice([Constants.SNAKE_MOVE_RIGHT, Constants.SNAKE_MOVE_LEFT, Constants.SNAKE_MOVE_FORWARD])
-    return TreeNode(terminal)
+    return random.choice([Constants.SNAKE_MOVE_RIGHT, Constants.SNAKE_MOVE_LEFT, Constants.SNAKE_MOVE_FORWARD])
+
+
+def get_random_terminal_tree_node():
+    return TreeNode(get_random_terminal())
 
 
 def get_random_function(game):
-    choice = random.choice(range(6))
+    choice = random.randint(0, 5)
     if choice == 0:
-        fnc = game.if_food_forward
+        return game.if_food_forward
     elif choice == 1:
-        fnc = game.if_food_left
+        return game.if_food_left
     elif choice == 2:
-        fnc = game.if_food_right
+        return game.if_food_right
     elif choice == 3:
-        fnc = game.if_obstacle_forward
+        return game.if_obstacle_forward
     elif choice == 4:
-        fnc = game.if_obstacle_left
+        return game.if_obstacle_left
     else:
-        fnc = game.if_obstacle_right
-    return TreeNode(fnc)
+        return game.if_obstacle_right
+
+
+def get_random_function_tree_node(game):
+    return TreeNode(get_random_function(game))
+
+
+def count_nodes(tree):
+    count = 1
+    if tree.left is None:
+        return count
+    count += count_nodes(tree.left)
+    count += count_nodes(tree.right)
+    return count
--- GeneticProgramming.py
+++ GeneticProgramming.py
@@ -4,8 +4,11 @@ import NoUI
 import Game
 import GeneticController
 import pyglet
+import random
 
 size_of_population = 20
+num_of_offsprings = 20
+mutation_rate = 0.1
 
 
 class GeneticProgramming:
@@ -19,11 +22,12 @@ class GeneticProgramming:
         self.layout = tuple([2, 4])
         self.game_width = self.screen_width // self.layout[1]
         self.game_height = self.screen_height // self.layout[0]
+        self.square_size = 20
         self.ui = SimpleUI.ControlPaneUI(self.x, self.y + (self.layout[0] - 1) * self.game_height,
                                          self.game_width, self.game_height)
         self.population = []
         self.randomly_initialize_population()
-        self.run()
+        self.test_fitness()
 
     def randomly_initialize_population(self):
         # first few individuals which will draw themselves
@@ -31,20 +35,20 @@ class GeneticProgramming:
             for j in range(self.layout[1]):
                 if i != (self.layout[0] - 1) or j != 0:
                     ui = SimpleUI.SimpleUI(self.x + j * self.game_width, self.y + i * self.game_height,
-                                           self.game_width, self.game_height, 20)
+                                           self.game_width, self.game_height, self.square_size)
                     game = Game.Game(ui)
                     controller = GeneticController.GeneticController(game)
                     self.population.append(controller)
         # rest of the population that will not draw itself (in the beginning)
         for i in range(size_of_population - ((self.layout[0] * self.layout[1]) - 1)):
             ui = NoUI.NoUI(0, 0,  # some dummy values
-                           self.game_width, self.game_height, 20)
+                           self.game_width, self.game_height, self.square_size)
             game = Game.Game(ui)
             controller = GeneticController.GeneticController(game)
             self.population.append(controller)
 
-    def run(self):
-        self.still_running = self.population
+    def test_fitness(self):
+        self.still_running = self.population.copy()
         pyglet.clock.schedule_interval(self.make_next_move_with_all, 1 / 15)
 
     def make_next_move_with_all(self, dt):
@@ -52,6 +56,7 @@ class GeneticProgramming:
         for individual in self.still_running:
             individual.make_next_move()
             if individual.state is not Constants.PLAY:
+                self.population[self.population.index(individual)].game.score = individual.game.score
                 self.still_running.remove(individual)
                 if len(self.still_running) >= 7 and isinstance(individual.game.ui, SimpleUI.SimpleUI):
                     self.still_running[last_draw_index].game.ui = SimpleUI.SimpleUI(
@@ -63,3 +68,53 @@ class GeneticProgramming:
         if len(self.still_running) == 0:
             self.state = Constants.WIN  # not PLAY
             pyglet.clock.unschedule(self.make_next_move_with_all)
+            self.move_to_next_generation()
+
+    def move_to_next_generation(self):
+        index_sum = ((size_of_population - 1) * size_of_population) // 2
+        self.population.sort(key=self.get_fitness)
+        self.substitute_population()
+        offsprings = []
+        for i in range(num_of_offsprings):
+            parent1 = self.select_parent(index_sum)
+            parent2 = self.select_parent(index_sum)
+            offs1, offs2 = parent1.crossover(parent2)
+            if random.random() <= mutation_rate:
+                offs1.mutate()
+            if random.random() <= mutation_rate:
+                offs2.mutate()
+            offsprings.append(offs1)
+            offsprings.append(offs2)
+        self.population += offsprings
+        self.test_fitness()
+
+    def select_parent(self, index_sum):
+        tmp = 0
+        r = random.randint(0, index_sum)
+        for i in range(size_of_population - 1, 0, -1):
+            tmp += i
+            if r <= tmp:
+                return self.population[i]
+
+    def get_fitness(self, individual):
+        return individual.game.score
+
+    def substitute_population(self):
+        i = 0
+        for individual in self.population:
+            if i < size_of_population:
+                individual.game = Game.Game(NoUI.NoUI(0, 0, self.game_width, self.game_height, self.square_size))
+            else:
+                del self.population[size_of_population - 1:]
+                break
+
+        # first few individuals which will draw themselves
+        index = 0
+        for i in range(self.layout[0]):
+            for j in range(self.layout[1]):
+                if i != (self.layout[0] - 1) or j != 0:
+                    ui = SimpleUI.SimpleUI(self.x + j * self.game_width, self.y + i * self.game_height,
+                                           self.game_width, self.game_height, self.square_size)
+                    self.population[index].game.ui = ui
+                    index += 1
+
