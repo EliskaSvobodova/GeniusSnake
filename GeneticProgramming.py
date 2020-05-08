bf13ee8f9bef043e161ffb8c3d3ae0b2705d87d6
svoboel5 <svoboel5@fit.cvut.cz>
2020-05-08 21:57:09 +0200

    Move reseting of games, fitness-based selection

--- GeneticProgramming.py
+++ GeneticProgramming.py
@@ -8,6 +8,7 @@ import random
 
 size_of_population = 50
 num_of_offsprings = 40
+num_of_random = 10
 mutation_rate = 0.1
 
 
@@ -39,7 +40,7 @@ class GeneticProgramming:
                     ui = SimpleUI.SimpleUI(self.x + j * self.game_width, self.y + i * self.game_height,
                                            self.game_width, self.game_height, self.square_size)
                     game = Game.Game(ui)
-                    controller = GeneticController.GeneticController(game, GeneticController.generate_test_tree(game))
+                    controller = GeneticController.GeneticController(game)
                     self.population.append(controller)
         # rest of the population that will not draw itself (in the beginning)
         for i in range(size_of_population - ((self.layout[0] * self.layout[1]) - 1)):
@@ -50,6 +51,18 @@ class GeneticProgramming:
             self.population.append(controller)
 
     def test_fitness(self):
+        for individual in self.population:
+            individual.game = Game.Game(NoUI.NoUI(0, 0, self.game_width, self.game_height, self.square_size))
+        index = 0
+        for i in range(self.layout[0]):
+            for j in range(self.layout[1]):
+                if i != (self.layout[0] - 1) or j != 0:
+                    ui = SimpleUI.SimpleUI(self.x + j * self.game_width, self.y + i * self.game_height,
+                                           self.game_width, self.game_height, self.square_size)
+                    game = self.population[index].game
+                    game.ui = ui
+                    game.ui.redraw(game.snake, game.score, game.apple)
+                    index += 1
         self.still_running = self.population.copy()
         #pyglet.clock.schedule_interval(self.make_next_move_with_all, 1 / 20)
         pyglet.clock.schedule(self.make_next_move_with_all)
@@ -75,17 +88,19 @@ class GeneticProgramming:
     def move_to_next_generation(self):
         print("-----------------------------------------------------------")
         print(f"Generation {self.generation}")
-        index_sum = ((size_of_population - 1) * size_of_population) // 2
+        #index_sum = ((size_of_population - 1) * size_of_population) // 2
         self.population.sort(key=self.get_fitness)
         print("Best individual: ", end="")
         self.population[len(self.population)-1].root.print()
         print()
         print("Score: ", self.population[len(self.population)-1].game.score)
+        # print("Population:")
         self.substitute_population()
+        fitness_sum = sum([i.game.score for i in self.population])
         offsprings = []
         for i in range(num_of_offsprings):
-            parent1 = self.select_parent(index_sum)
-            parent2 = self.select_parent(index_sum)
+            parent1 = self.select_parent(fitness_sum)
+            parent2 = self.select_parent(fitness_sum)
             offs1, offs2 = parent1.crossover(parent2)
             if random.random() <= mutation_rate:
                 offs1.mutate()
@@ -94,11 +109,16 @@ class GeneticProgramming:
             offsprings.append(offs1)
             offsprings.append(offs2)
         self.population += offsprings
+        for i in range(num_of_random):
+            ui = NoUI.NoUI(0, 0, self.game_width, self.game_height, self.square_size)
+            game = Game.Game(ui)
+            controller = GeneticController.GeneticController(game)
+            self.population.append(controller)
         self.generation += 1
         self.ui.draw(self.generation)
         self.test_fitness()
 
-    def select_parent(self, index_sum):
+    def select_parent_rank(self, index_sum):
         tmp = 0
         r = random.randint(0, index_sum)
         for i in range(size_of_population - 1, 0, -1):
@@ -106,34 +126,23 @@ class GeneticProgramming:
             if r <= tmp:
                 return self.population[i]
 
+    def select_parent(self, fitness_sum):
+        tmp = 0
+        r = random.randint(0, fitness_sum)
+        for individual in self.population:
+            tmp += individual.game.score
+            if r <= tmp:
+                return individual
+
     def get_fitness(self, individual):
         return individual.game.score
 
     def substitute_population(self):
-        i = 0
-        next_population = []
         self.population.reverse()
-        for individual in self.population:
-            # print("Score: ", individual.game.score)
-            individual.game = Game.Game(NoUI.NoUI(0, 0, self.game_width, self.game_height, self.square_size))
-            next_population.append(individual)
-            # individual.root.print()
-            # print()
-            # print()
-            i += 1
-            if i >= size_of_population:
-                break
-        self.population = next_population
-
-        # first few individuals which will draw themselves
-        index = 0
-        for i in range(self.layout[0]):
-            for j in range(self.layout[1]):
-                if i != (self.layout[0] - 1) or j != 0:
-                    ui = SimpleUI.SimpleUI(self.x + j * self.game_width, self.y + i * self.game_height,
-                                           self.game_width, self.game_height, self.square_size)
-                    game = self.population[index].game
-                    game.ui = ui
-                    game.ui.redraw(game.snake, game.score, game.apple)
-                    index += 1
+        del self.population[size_of_population:]
+        # for i in self.population:
+        #     print("Score: ", i.game.score)
+        #     i.root.print()
+        #     print()
+        #     print()
 
